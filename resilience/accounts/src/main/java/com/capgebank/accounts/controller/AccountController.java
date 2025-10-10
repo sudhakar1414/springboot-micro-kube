a5492603be340ae6019f1ceb0c306406205d9434
sudhakar gutti <sudhakar8324@gmail.com>
2025-10-10 21:07:25 +0530

    rate limiter pattern in accounts

--- resilience/accounts/src/main/java/com/capgebank/accounts/controller/AccountController.java
+++ resilience/accounts/src/main/java/com/capgebank/accounts/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.capgebank.accounts.dto.CustomerDto;
 import com.capgebank.accounts.dto.ErrorResponseDto;
 import com.capgebank.accounts.dto.ResponseDto;
 import com.capgebank.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -231,9 +232,15 @@ public class AccountController {
             )
     })
     @GetMapping("/java-version")
+    @RateLimiter(name = "getJavaVersion",fallbackMethod = "getJavaVersionFallback")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
