c4b79229a061d68ea17b0b0c896b5e9c5821ee54
sudhakar gutti <sudhakar8324@gmail.com>
2025-10-10 15:57:27 +0530

    circuit breaker design pattern implementation

---
+++ resilience/accounts/src/main/java/com/capgebank/accounts/service/CustomerServiceImpl.java
@@ -0,0 +1,49 @@
+package com.capgebank.accounts.service;
+
+import com.capgebank.accounts.dto.AccountsDto;
+import com.capgebank.accounts.dto.CardsDto;
+import com.capgebank.accounts.dto.CustomerDetailsDto;
+import com.capgebank.accounts.dto.LoansDto;
+import com.capgebank.accounts.exception.ResourceNotFoundException;
+import com.capgebank.accounts.mapper.AccountsMapper;
+import com.capgebank.accounts.mapper.CustomerMapper;
+import com.capgebank.accounts.model.Account;
+import com.capgebank.accounts.model.Customer;
+import com.capgebank.accounts.repository.AccountsRepository;
+import com.capgebank.accounts.repository.CustomerRepository;
+import com.capgebank.accounts.service.client.CardsFeignClient;
+import com.capgebank.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerServiceImpl implements ICustomerService{
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber,String correlationId) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+        Account account = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString()));
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer,new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(account,new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId,mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId,mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        return customerDetailsDto;
+    }
+}
---
+++ resilience/accounts/src/main/java/com/capgebank/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,16 @@
+package com.capgebank.accounts.service.client;
+
+import com.capgebank.accounts.dto.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("cards")
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("capgebank-correlation-id") String correlationId,
+            @RequestParam String mobileNumber);
+}
