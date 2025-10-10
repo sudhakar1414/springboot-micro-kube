5bf2728f0869000f6e9a5165ad218c9afeabfbaa
sudhakar gutti <sudhakar8324@gmail.com>
2025-10-10 13:30:39 +0530

    correlation id implementation for all services

--- edgeserver/accounts/src/main/java/com/capgebank/accounts/controller/CustomerController.java
+++ edgeserver/accounts/src/main/java/com/capgebank/accounts/controller/CustomerController.java
@@ -10,14 +10,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,6 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
         description = "CRUD REST APIs in CapgeBank to fetch customer details"
 )
 public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     private final ICustomerService iCustomerService;
 
@@ -55,10 +56,12 @@ public class CustomerController {
             )
     })
     @GetMapping("/fetchCustomerDetails")
-    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("capgebank-correlation-id") String correlationId,
+                                                                   @RequestParam
                                                                    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
                                                                    String mobileNumber ){
-        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber);
+        logger.debug("capgeBank-correlation-id found: {}",correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber,correlationId);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(customerDetailsDto);
     }
