bfc28fd33c70af136a3fc3b50e012a2ac8373e90
sudhakar gutti <sudhakar8324@gmail.com>
2025-10-13 23:17:01 +0530

    monitoring services

--- monitoring/accounts/src/main/java/com/capgebank/accounts/service/ICustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.capgebank.accounts.service;
-
-import com.capgebank.accounts.dto.CustomerDetailsDto;
-
-public interface ICustomerService {
-
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber,String correlationId);
-
-}
